0e7f6ebddeb8cec1c8a8cad4c81c9d26083e4faa
ptrv-dev <petrov99119@gmail.com>
2023-07-16 14:34:29 +0300

    application port changed

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.setViewEngine('hbs');
-  await app.listen(3000);
+  await app.listen(5000);
 }
 bootstrap();
