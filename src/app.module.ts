84df33c4eea8d7496e295598a9d0f567b78b61db
ptrv-dev <petrov99119@gmail.com>
2023-07-16 14:34:16 +0300

    postgres typeorm init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
+import User from './user/entity/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [User],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
   controllers: [],
   providers: [],
 })
