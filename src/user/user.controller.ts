8cc8f45c058630624fc30f8ddd6ec26ab05d30b7
ptrv-dev <petrov99119@gmail.com>
2023-07-16 16:10:55 +0300

    user: CRUD

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Get()
+  async getAll() {
+    const users = await this.userService.getAll();
+    return { data: users, message: '' };
+  }
+
+  @Get(':id')
+  async getOneById(@Param('id') id: string) {
+    const user = await this.userService.getOneById(parseInt(id));
+    return { data: user, message: '' };
+  }
+
+  @Post()
+  async create(@Body() dto: CreateUserDto) {
+    const user = await this.userService.create(dto);
+    return { data: user, message: '' };
+  }
+
+  @Patch(':id')
+  async updateOneById(@Param('id') id: string, @Body() dto: UpdateUserDto) {
+    const user = await this.userService.updateOneById(parseInt(id), dto);
+    return { data: user, message: '' };
+  }
+
+  @Delete(':id')
+  async removeOneById(@Param('id') id: string) {
+    const user = await this.userService.removeOneById(parseInt(id));
+    return { data: user, message: '' };
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entity/user.entity';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+  ) {}
+
+  async getAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async getOneById(id: number): Promise<User> {
+    const user = await this.userRepository.findOneBy({ id: id });
+    if (!user)
+      throw new NotFoundException("User with specified ID doesn't exist!");
+    return user;
+  }
+
+  async create(dto: CreateUserDto): Promise<User> {
+    const { email, password, username } = dto;
+    const exist = await this.userRepository.findOneBy({ email });
+    if (exist)
+      throw new ConflictException('Specified email address already taken!');
+    const user = this.userRepository.save({ username, password, email });
+    return user;
+  }
+
+  async removeOneById(id: number): Promise<User> {
+    const user = await this.userRepository.findOneBy({ id });
+    if (!user)
+      throw new NotFoundException("User with specified ID doesn't exist!");
+    await this.userRepository.remove(user);
+    return user;
+  }
+
+  async updateOneById(id: number, dto: UpdateUserDto): Promise<User> {
+    const user = await this.userRepository.findOneBy({ id });
+    if (!user)
+      throw new NotFoundException("User with specified ID doesn't exist!");
+
+    const { email, password, username } = dto;
+
+    if (email) {
+      const exist = await this.userRepository.findOneBy({ email });
+      if (exist && exist.id !== user.id)
+        throw new ConflictException('Specified email address already taken!');
+    }
+
+    Object.assign(user, { email, password, username });
+
+    const result = await this.userRepository.save(user);
+    return result;
+  }
+}
